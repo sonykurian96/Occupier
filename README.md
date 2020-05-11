712572102d5a4195d385f2bfdbb4f6315b684805
Justin Spegele <justin.spegele@gmail.com>
2020-05-11 17:47:51 -0400

    Initial commit. Working prototype with channels, DMs, channel favorites, basic search, notifications

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slack Clone
+
+Coming soon
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import firebase from './firebase'
+import App from './components/App';
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Spinner from './spinner'
+import rootReducer from './reducers';
+import { setUser, clearUser } from './actions'
+import * as serviceWorker from './serviceWorker'
+import 'semantic-ui-css/semantic.min.css'
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+class Root extends React.Component {
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user)
+        this.props.history.push('/')
+      } else {
+        this.props.history.push('/login')
+        this.props.clearUser()
+      }
+    })
+  }
+  render() {
+    return this.props.isLoading ? <Spinner /> : (
+      <Switch>
+        <Route exact={true} path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(
+  connect(mapStateToProps, { setUser, clearUser })(Root)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister()
