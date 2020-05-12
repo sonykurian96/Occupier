d735677cf311d31172572db03ab84c1fd1517813
Justin Spegele <justin.spegele@gmail.com>
2020-05-12 12:34:00 -0400

    Added primary/secondary color selector

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -51,9 +51,27 @@ const channel_reduer = (state = initialChannelState, action) => {
   }
 }
 
+const initialColorsState = {
+  primaryColor: '#4c3c4c',
+  secondaryColor: '#eee'
+}
+
+const colors_reducer = (state = initialColorsState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_COLORS:
+      return {
+        primaryColor: action.payload.primaryColor,
+        secondaryColor: action.payload.secondaryColor
+      }
+    default:
+      return state
+  }
+}
+
 const rootReducer = combineReducers({
   user: user_reducer,
-  channel: channel_reduer
+  channel: channel_reduer,
+  colors: colors_reducer
 })
 
 export default rootReducer
