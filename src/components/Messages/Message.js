3659d8197b3bda565bbf81b479eb1628851c6c68
sonykurian96 <sonykurian96@gmail.com>
2022-04-19 20:28:41 +0530

    second commit

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -10,8 +10,13 @@ const isImage = (message) => {
   return message.hasOwnProperty('image') && !message.hasOwnProperty('content')
 }
 
+const isCode = (message) =>{
+  return typeof message.content === 'string'? message.content.includes("code"):false 
+}
+
 const timeFromNow = timestamp => moment(timestamp).fromNow()
 
+const sentence = "code-js: console.log('hello world')99"
 const Message = ({ message, user }) => (
   <Comment>
     <Comment.Avatar src={message.user.avatar} />
@@ -19,11 +24,22 @@ const Message = ({ message, user }) => (
       <Comment.Author as="a">{message.user.name}</Comment.Author>
       <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
       {isImage(message) ?
-        <Image src={message.image} className="message__image" /> :
+        <Image src={message.image} className="message__image" /> : isCode(message)? <Image src={require("./sonyTA.png")} className="message__image" />:
         <Comment.Text>{message.content}</Comment.Text>
       }
+      {console.log(isCode(message))}
+
     </Comment.Content>
   </Comment>
 )
 
 export default Message
+
+// {isImage(message) ?
+//   <Image src={message.image} className="message__image" /> :
+//   <Comment.Text>{message.content}</Comment.Text>
+// }
+
+
+
+
